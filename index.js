ab32864969f5c04fca6458b3653159502f48e994
Shelli <viethoang5588@gmail.com>
2019-04-11 03:27:19 +0700

    Removing usage of firebase and use featherjs instead

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-var firebaseCredentials = require("./firebase-credentials.json");
-var admin = require('firebase-admin');
-// var db = admin.database();
+const Discord = require('discord.io');
+const logger = require('winston');
+const auth = require('./auth.json');
+const feathers = require('@feathersjs/feathers');
+const feathersApp = feathers();
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -11,23 +10,6 @@ logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
-
-// Firebase
-
-var fapp = admin.initializeApp({
-  credential: admin.credential.cert(firebaseCredentials),
-  databaseURL: 'https://partyfind-639e0.firebaseio.com'
-});
-
-var db = admin.firestore();
-
-// var et = db.ref('/et');
-// var val = db.ref('/val')
-// var oracle = db.ref('/oracle')
-
-// et.once("value", function(snapshot) {
-//   console.log(snapshot.val());
-// });
 
 // Initialize Discord Bot
 var bot = new Discord.Client({
@@ -64,17 +46,32 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     logger.info('Instance: ' + instance);
                     switch(instance) {
                         case 'ET': 
+                            //^create ET [date+time] (ROMChannel) #ET-1 {roles}
+                            // roles: are optional (default: TANK PRIEST DPS DPS DPS)
                             bot.sendMessage({
                                 to: channelID,
                                 message: 'Creating ET party'
                             })
 
-                            var date = args[2];
-                            var channel = args[3];
-                            bot.sendMessage({
-                                to: channelID,
-                                message: 'ET ' + '[' + date + ']' + ' (' + channel + ')'
-                            })
+                            var dateTime = args[2]; // just string
+                            var romChannel = args[3]; // eg: EN14
+                            var discordChannel = args[4]; // #ET-1
+                            var roles = [];
+
+                            if (args.length > 5) {
+                                roles.push(args[5]);
+                                roles.push(args[6]);
+                                roles.push(args[7]);
+                                roles.push(args[8]);
+                                roles.push(args[9]);
+                            }
+
+                            createET(channelID, dateTime, romChannel, discordChannel, roles);
+                        
+                            // bot.sendMessage({
+                            //     to: channelID,
+                            //     message: 'ET ' + '[' + dateTime + ']' + ' (' + romChannel + ')'
+                            // })
                         break;
                         default:
                             bot.sendMessage({
@@ -90,3 +87,116 @@ bot.on('message', function (user, userID, channelID, message, evt) {
     }
     
 });
+
+async function createET(channelID, date, romChannel, discordChannel, roles) {
+    var newEt;
+    if (roles.length > 0) {
+        newEt = {
+          date: date,
+          romChannel: romChannel,
+          discordChannel: discordChannel,
+          role1Name: roles[0],
+          role2Name: roles[1],
+          role3Name: roles[2],
+          role4Name: roles[3],
+          role5Name: roles[4]
+        }
+    } else {
+        newEt = {
+            date: date,
+            romChannel: romChannel,
+            discordChannel: discordChannel,
+            role1Name: 'TANK',
+            role2Name: 'PRIEST',
+            role3Name: 'DPS',
+            role4Name: 'DPS',
+            role5Name: 'DPS'
+          }
+    } 
+
+    await app.service('et').create({
+        date: date,
+        romChannel: romChannel,
+        discordChannel: discordChannel,
+    });
+
+    bot.sendMessage({
+        to: channelID,
+        message: 'ET ' + '[' + dateTime + ']' + ' (' + romChannel + ') $(discordChannel) \n' +
+                 '1. $(role1Name) \n' + 
+                 '2. $(role2Name) \n' +
+                 '3. $(role3Name) \n' +
+                 '4. $(role4Name) \n' +
+                 '5. $(role5Name) \n'
+    })
+}
+
+// Setup feathers 
+feathersApp.use('et', {
+    // async get(id) {
+    //     return {
+    //         id, 
+    //         date, 
+    //         romChannel,
+    //         discordChannel,
+    //         comment,
+    //         role1Name,
+    //         role1User,
+    //         role2Name, 
+    //         role2User,
+    //         role3Name,
+    //         role3User,
+    //         role4Name, 
+    //         role4User,
+    //         role5Name,
+    //         role5User,
+    //     }
+    // }
+})
+
+class ET {
+    constructor() {
+        this.ets = [];
+        this.currentId = 0;
+    }
+
+    async find(params) {
+        return this.ets;
+    }
+
+    async get(id, params) {
+        const et = this.ets.find(et => et.id === parseInt(id, 10));
+
+        if (!et) {
+            throw new Error('No ET instance found with id ${id}');
+        }
+
+        return et;
+    }
+
+    async create(data, params) {
+        const et = Object.assign({
+            id: ++this.currentId
+        }, data);
+
+        this.ets.push(et);
+
+        return et;
+    }
+
+    async patch(id, data, params) {
+        const et = await this.get(id);
+
+        return Object.assign(et, data);
+    }
+
+    async remove(id, params) {
+        const et = await this.get(id);
+
+        const index = this.ets.indexOf(et);
+
+        this.ets.splice(index, 1);
+
+        return et;
+    }
+}
